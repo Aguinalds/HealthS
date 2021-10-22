6f45f41d6fb655abeab23bc3fe88ec37136ad59f
Aguinaldo <guimachado1708@gmail.com>
2021-10-21 23:55:31 -0300

    Add Header, footer, admin, sobre

--- HealthStar/src/app/app.module.ts
+++ HealthStar/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+import { AdminComponent } from './admin/admin.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    NavComponent,
+    AdminComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
